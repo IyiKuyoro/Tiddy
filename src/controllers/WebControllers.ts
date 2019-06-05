824adda278543e026d2ec90fe139356757f4817e
IyiKuyoro <opeoluwa.iyikuyoro@gmail.com>
2019-06-05 10:49:31 +0100

    feat(install): add functionality to install app

--- src/controllers/WebControllers.ts
+++ src/controllers/WebControllers.ts
@@ -1,8 +1,19 @@
+import axios from "axios";
+import qs from 'qs';
+
+import { Logger } from "..//logger";
+import config from '../config';
+
 class WebController {
   public static RenderHomePage(req: any, res: any) {
     res.render('home', {
+      slackButtonHref: '/install',
       title: 'Install Tiddy',
     });
+  }
+
+  public static InstallRedirect(req: any, res: any) {
+    res.redirect(WebController.slackInstallationUrl);
   }
 
   public static RenderPrivacyPage(req: any, res: any) {
@@ -16,6 +27,35 @@ class WebController {
       title: 'Support',
     });
   }
+
+  public static async Authorize(req: any, res: any) {
+    try {
+      const response = await axios.post('https://slack.com/api/oauth.access', qs.stringify({
+        client_id: config.SLACK_CLIENT_ID,
+        client_secret: config.SLACK_CLIENT_SECRET,
+        code: req.query.code,
+      }), {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+      });
+
+      if (!response.data.ok) {
+        throw new Error(`Could not install app. ${response.data.error}`);
+      }
+
+      res.render('installationSuccess', {
+        title: 'Success',
+      })
+    } catch (error) {
+      Logger.error(error);
+      res.render('installationError', {
+        title: 'Error',
+      })
+    }
+  }
+
+  private static slackInstallationUrl: string = `https://slack.com/oauth/authorize?client_id=${config.SLACK_CLIENT_ID}&scope=admin,bot,commands`;
 }
 
 export default WebController;
--- src/routes/web.ts
+++ src/routes/web.ts
@@ -4,7 +4,9 @@ import WebController from '../controllers/WebControllers';
 const webRouter = Router();
 
 webRouter.get('/', WebController.RenderHomePage)
+webRouter.get('/install', WebController.InstallRedirect)
 webRouter.get('/privacy', WebController.RenderPrivacyPage)
 webRouter.get('/support', WebController.RenderSupportPage)
+webRouter.get('/auth/authorize', WebController.Authorize)
 
 export default webRouter;
