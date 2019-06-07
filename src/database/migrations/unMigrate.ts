fd22a7d8e98d3692d247a6f3caaaf8622f2f5f42
Tomi Adebanjo <tomiadebanjo@gmail.com>
2019-06-07 11:46:04 +0100

    chore(database): implement database rollback functionality
    - create script to rollback migrations
    - add 'un-migrate' command to package.json scripts
    
    [Finishes #166484041]

---
+++ src/database/migrations/unMigrate.ts
@@ -0,0 +1,36 @@
+import { Client } from 'pg';
+
+import config from '../../config';
+import { Logger } from '../../logger';
+
+const client = new Client({
+  database: config.DATABASE,
+  host: config.DATABASE_HOST,
+  password: config.DATABASE_PASSWORD,
+  port: 5432,
+  user: config.DATABASE_USERNAME,
+});
+
+client.connect();
+
+const dropTable = `
+DO $$ DECLARE
+  r RECORD;
+BEGIN
+    FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = current_schema()) LOOP
+        EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(r.tablename) || ' CASCADE';
+    END LOOP;
+END $$;
+`;
+
+client
+  .query(dropTable)
+  .then(() => {
+    Logger.info('Un-migration successful');
+    process.exit(0);
+    client.end();
+  })
+  .catch(e => {
+    Logger.error(e);
+    process.exit(1);
+  });
