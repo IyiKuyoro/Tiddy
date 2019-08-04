095ca8b93c165638f8dacef14321e4bb98df89b6
IyiKuyoro <opeoluwa.iyikuyoro@gmail.com>
2019-08-04 07:57:26 +0100

    chore(deployment): fix migration issues

--- scripts/heroku-prebuild.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# This script helps Move Ambient Types (mat) into the node_modules after once instalation runs.
-
-# Check that there is a node modules folder
-export NODE_ENV=production
-
-npm run setup && npm run migrate
